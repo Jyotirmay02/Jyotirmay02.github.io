2b37055588a5fed06eac780857de5ee32921a70a
Jyotirmay02 <jyotirmays123@gmail.com>
2021-08-09 19:32:47 +0530

    initial commit

---
+++ profile.js
@@ -0,0 +1,120 @@
+var h1 = document.querySelector("#one")
+
+// h1.addEventListener('mouseover',function(){
+//     h1.style.fontSize = "22px";
+//     h1.style.color = 'white';
+// })
+// h1.addEventListener('mouseout',function(){
+//     h1.style.fontSize = "20px";
+//     h1.style.color = 'darkgray';
+// })
+
+// var h7 = document.querySelector("#seven")
+
+// h7.addEventListener('mouseover',function(){
+//     // h7.style.fontSize = "22px";
+//     h7.style.color = 'white';
+// })
+// h7.addEventListener('mouseout',function(){
+//     // h7.style.fontSize = "20px";
+//     h7.style.color = 'darkgray';
+// })
+
+// var h8 = document.querySelector("#eight")
+
+// h8.addEventListener('mouseover',function(){
+//     // h8.style.fontSize = "22px";
+//     h8.style.color = 'white';
+// })
+// h8.addEventListener('mouseout',function(){
+//     h8.style.fontSize = "20px";
+//     h8.style.color = 'darkgray';
+// })
+
+var h1 = document.querySelector("#one")
+
+h1.addEventListener('mouseover',function(){
+    // h1.style.fontSize = "22px";
+    h1.style.color = 'white';
+})
+h1.addEventListener('mouseout',function(){
+    // h1.style.fontSize = "20px";
+    h1.style.color = 'darkgray';
+})
+
+var h2 = document.querySelector("#two")
+
+h2.addEventListener('mouseover',function(){
+    // h2.style.fontSize = "22px";
+    h2.style.color = 'white';
+})
+h2.addEventListener('mouseout',function(){
+    // h2.style.fontSize = "20px";
+    h2.style.color = 'darkgray';
+})
+
+var h3 = document.querySelector("#three")
+
+h3.addEventListener('mouseover',function(){
+    // h3.style.fontSize = "22px";
+    h3.style.color = 'white';
+})
+h3.addEventListener('mouseout',function(){
+    // h3.style.fontSize = "20px";
+    h3.style.color = 'darkgray';
+})
+
+var h4 = document.querySelector("#four")
+
+h4.addEventListener('mouseover',function(){
+    // h4.style.fontSize = "22px";
+    h4.style.color = 'white';
+})
+h4.addEventListener('mouseout',function(){
+    // h4.style.fontSize = "20px";
+    h4.style.color = 'darkgray';
+})
+
+
+var h5 = document.querySelector("#five")
+
+h5.addEventListener('mouseover',function(){
+    // h5.style.fontSize = "22px";
+    h5.style.color = 'white';
+})
+h5.addEventListener('mouseout',function(){
+    // h5.style.fontSize = "20px";
+    h5.style.color = 'darkgray';
+})
+
+
+var h6 = document.querySelector("#six")
+
+h6.addEventListener('mouseover',function(){
+    // h6.style.fontSize = "22px";
+    h6.style.color = 'white';
+})
+h6.addEventListener('mouseout',function(){
+    // h6.style.fontSize = "20px";
+    h6.style.color = 'darkgray';
+})
+
+// $(".one").mouseover();
+
+// $( ".one" )
+//   .mouseenter(function() {
+//     n += 1;
+//     $( this ).find( "li" ).style.color = 'red';
+//   })
+//   .mouseleave(function() {
+//     $( this ).find( "li" ).style.color = 'darkgray';
+//   });
+
+// var h = document.querySelector(".one li")
+
+// Array.from(document.querySelectorAll('.one')).forEach(function(ele){
+//     if(ele.querySelector('li')){
+//         ele.style.color = 'red';
+//     }
+// });
+
